64d37eed9f1673f986a4b08d0ebaa9c4a269f41c
Quantum-Codes <87054411+Quantum-Codes@users.noreply.github.com>
2025-05-20 21:55:40 +0530

    cleaned rank vs roll data

--- main.py
+++ main.py
@@ -31,38 +31,119 @@ number_of_pages = len(reader.pages)
 
 
 # Roll vs CRL
-page_nos = range(53, 212) # until AAT text
+page_nos = range(200, 212) # until and including AAT text
 page_content = []
-headers = [] # to make all table headers appear only once
+headers = [] # to make all table headers appear only once. 
+aat = 0 # aat switch, if currently viewing aat then its 1
+prep = 0 # prep switch, if currently viewing prep then its 1. cuz prepcourse ranks are joined with roll numbers
+"""
+noise appear only for PREP courses as:
+(All these inconsistent entries start with line "Roll NoPREP-" and continue till the first numeric entry)
+Roll NoPREP-
+OBC-
+PwDRoll NoPREP-
+OBC-
+PwDRoll NoPREP-
+OBC-
+PwDRoll NoPREP-
+OBC-
+PwDRoll NoPREP-
+OBC-
+PwDRoll NoPREP-
+OBC-
+PwD
+
+or
+
+Roll NoPREP-
+SC-PwDRoll NoPREP-
+SC-PwDRoll NoPREP-
+SC-PwDRoll NoPREP-
+SC-PwDRoll NoPREP-
+SC-PwDRoll NoPREP-
+SC-PwD
+"""
 for i in page_nos:
     content = reader.pages[i].extract_text()
     content = content.split("\n")
-    content.pop(0)
-    content.pop(-1)
-    print("\n".join(content))
+    if i == 53:
+        del content[0:3]
+    else:
+        content.pop(0)
     
-    # for line_no, line in enumerate(content):
-    #     if "Aggregate" in line:
-    #         content[line_no] = "deleted"
-    #         continue
+    content.pop(-1)
+    
+    for line_no, line in enumerate(content): 
+        if line == "deleted":
+            continue
+        if "Rank" in line:
+            content[line_no] = "deleted"
+            continue
         
-    #     for id, char in enumerate(line):
-    #         if char.isalpha():
-    #             if id == 0:
-    #                 if line in headers:
-    #                     content[line_no] = "deleted"
-    #                     break
-    #                 headers.append(line)
-    #                 break # normal table header line like 'CRL Score'
-    #             new_items = ( line[:id], line[id:] )
-    #             content[line_no] = new_items[0]
-    #             content.insert(line_no + 1, new_items[1])
-    #             break
+        # handle AAT roll numbers
+        if "AAT" in line or aat == 1:
+            aat = 1
+            break 
+        
+        if "Roll" in line:
+            if "NoPREP-" in line: # malformed line group
+                prep = 1
+                next_line = line
+                malformed_lines = line # contains all malformed lines joined together in a single string
+                tmp_line_no = line_no
+                while not next_line[0].isnumeric(): # loop till a numeric entry is found
+                    tmp_line_no += 1
+                    next_line = content[tmp_line_no]
+                    malformed_lines += next_line.strip() # remove newline
+                    content[tmp_line_no] = "deleted"
+                # we have now deleted all malformed lines and put them in malformed_lines
+                # now we need to detect the category 
+                malformed_lines = malformed_lines.split(" ")
+                category = malformed_lines[1][2:-4] # category is the second word in the malformed line
+                # to remove the 'No' infront of category and 'Roll' at end of it, we put [2:]
+                headers.append(category)
+                print(f"Header found: {category}")
+                # now we need to replace the malformed lines with the category
+                content[line_no] = category
+                continue
                 
-    # for i in range(content.count("deleted")):
-    #     content.remove("deleted")
-    # page_content.extend(content)
+                    
+                
+            line = line.split(" ")
+            if line[0] == "Adv": # for Prep courses, its written as just "roll no", so need to delete "Adv" for other categories
+                line.pop(0)
+            
+            category = line[2]
+            
+            if category in headers:
+                content[line_no] = "deleted"
+                print(f"Duplicate header found: {category}")
+                continue
+            headers.append(category)
+            print(f"Header found: {category}")
+            content[line_no] = category
+        
+        # handle ranks and roll numbers + fix entries for prep courses(rank joined with roll)
+        # roll number = 9 digit
+        # rank = upto 5 digit
+        if prep:
+            line = line.split(" ")
+            for j, item in enumerate(line):
+                if len(item) > 9: # joined rank and roll
+                    roll = item[-9:]
+                    rank = item[:-9]
+                    line[j] = rank
+                    line.insert(j + 1, roll)
+                    continue
+            content[line_no] = " ".join(line)
+                     
+        
+                
+    for i in range(content.count("deleted")):
+        content.remove("deleted")
+    page_content.extend(content)
 
+print("\n".join(page_content))
 
 
 
